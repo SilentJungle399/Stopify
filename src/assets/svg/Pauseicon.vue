e43e0b7f3249e32dead1aba2ca5ec601bdfecb4c
SilentJungle399 <silentjungle399@gmail.com>
2021-10-21 15:14:29 +0530

    icons cus ye

---
+++ src/assets/svg/Pauseicon.vue
@@ -0,0 +1,7 @@
+<template>
+	<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<rect x="4" y="3" width="5" height="14" fill="white"/>
+	<rect x="11" y="3" width="5" height="14" fill="white"/>
+	</svg>
+
+</template>
---
+++ src/assets/svg/Playicon.vue
@@ -0,0 +1,4 @@
+<template>
+	<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path class="playpath" d="M4 19.1169V2.8831C4 2.49445 4.42399 2.25439 4.75725 2.45435L18.2854 10.5713C18.6091 10.7655 18.6091 11.2345 18.2854 11.4287L4.75725 19.5457C4.42399 19.7456 4 19.5056 4 19.1169Z" stroke="white" stroke-width="1"/>
+	</svg>
+</template>
